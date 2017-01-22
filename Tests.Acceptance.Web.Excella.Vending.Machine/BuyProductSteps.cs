9720ee40a4d5d52d6dbe8fa0526a7e17ffe3359f
Sean Killeen <SeanKilleen@gmail.com>
2017-01-22 18:01:41 -0500

    Extract IISExpressTestManager into its own class

--- Tests.Acceptance.Web.Excella.Vending.Machine/BuyProductSteps.cs
+++ Tests.Acceptance.Web.Excella.Vending.Machine/BuyProductSteps.cs
@@ -1,66 +1,10 @@
 ﻿using System;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using NUnit.Framework;
 using TechTalk.SpecFlow;
 // ReSharper disable UnusedMember.Global -- test methods are said to be unused which isn't correct. -SK
 
 namespace Tests.Acceptance.Web.Excella.Vending.Machine
 {
-    public static class IISExpressTestManager
-    {
-        private const int IIS_PORT = 8484;
-        private const string APPLICATION_NAME = "Excella.Vending.Web.UI";
-
-        public static void StopIISExpress()
-        {
-            var localIISExpressProcesses = GetIISProcesses();
-            foreach (var iisExpressProcess in localIISExpressProcesses)
-            {
-                if (!iisExpressProcess.HasExited)
-                {
-                    iisExpressProcess.Kill();
-                }
-            }
-        }
-        private static Process[] GetIISProcesses()
-        {
-            return Process.GetProcessesByName("iisexpress");
-        }
-        public static bool IsIISExpressRunning()
-        {
-            var localIISExpressProcesses = GetIISProcesses();
-
-            return localIISExpressProcesses.Any(x => x.HasExited == false);
-
-        }
-        public static void StartIISExpress()
-        {
-            var applicationPath = GetApplicationPath(APPLICATION_NAME);
-            var programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFilesX86);
-            var startInfoFileName = programFiles + @"\IIS Express\iisexpress.exe";
-            var startInfoArguments = $"/config:\"{applicationPath}\\..\\..\\..\\.vs\\config\\applicationhost.config\"";
-
-            var iisProcess = new Process
-            {
-                StartInfo =
-                {
-                    FileName = startInfoFileName,
-                    Arguments = startInfoArguments
-                }
-            };
-
-            iisProcess.Start();
-        }
-        private static string GetApplicationPath(string applicationName)
-        {
-            var solutionFolder = Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
-            // ReSharper disable once AssignNullToNotNullAttribute -- test will fail if it's null.
-            return Path.Combine(solutionFolder, applicationName);
-        }
-
-    }
     [Binding]
     public class BuyProductSteps
     {
